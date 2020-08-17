4bc8d60414adc5880bd5444140ea76a2294718a8
biojet1 <biojet1@gmail.com>
2020-08-17 21:04:41 +0800

    media

--- hcmd/debug.py
+++
@@ -1,5 +0,0 @@
-def index(request):
-	from django.http import HttpResponse
-	import pprint
-	vars = {'GET' : request.GET, 'POST' : request.POST, 'META' : request.META, 'FILES' : request.FILES}
-	return HttpResponse(pprint.pformat(vars))
